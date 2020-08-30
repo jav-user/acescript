e8bc1dca41e292bd054088bbdbc18ecaa65c0f1f
javuser <javuser@gmail.com>
2020-08-30 15:07:55 -0500

    css

--- rarbg/torrent-vue/torrent-vue.images.component.js
+++ rarbg/torrent-vue/torrent-vue.images.component.js
@@ -15,14 +15,14 @@ Vue.component("vimages", {
 		<span v-for="(image, id) in images">
 			<input 
 				v-model="plugin" 
-				@keyup="poster(id)" 
-				v-bind:class="this.success ? 'input-success' : 'input-error'"/>
+				@change="poster(id)" 
+				v-bind:class="success ? 'input-success' : 'input-error'"/>
 				<br/>
 			<input v-model="image.src"/><br/>
 			<a :href="image.href" target="_blank">
 				<img 
 					:src="image.src" 
-					v-bind:class="this.success ? 'thumbnail' : 'poster'"
+					v-bind:class="success ? 'poster' : 'thumbnail'"
 					/>
 			</a>
 		</span>
@@ -30,21 +30,27 @@ Vue.component("vimages", {
 	data: function () {
 		return {
 			images: Torrent.images,
-			def_images: new nobj(Torrent.images).clone().exec(),
+			def_images: JSON.parse(JSON.stringify(Torrent.images)),
 			plugin: "src",
+			success: false,
 		};
 	},
 	methods: {
 		poster(id) {
+			// console.log(id, this.def_images[id].src);
 			var image = this.images[id];
+			image.src = this.def_images[id].src;
+			image.href = this.def_images[id].href;
 			var src = image.src;
 			try {
 				src = eval(this.plugin);
+				var Src = new URL(src);
+				console.log(image.src != src);
 				this.success = image.src != src;
 				image.src = src;
 				image.href = src;
 			} catch (err) {
-				console.log(err);
+				console.log("Error in plugin");
 				this.success = false;
 				image.src = this.def_images[id].src;
 				image.href = this.def_images[id].href;
