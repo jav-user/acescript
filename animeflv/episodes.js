d0fc85a4cdd2312863b5852fd4d822fd317ac0aa
javuser <javuser@gmail.com>
2020-10-02 13:18:04 -0500

    animeflv.net episodes

---
+++ animeflv/episodes.js
@@ -0,0 +1,83 @@
+// ==UserScript==
+// @name AnimeFlvEps
+// @namespace AnimeFlvEps Scripts
+// @match https://www3.animeflv.net/anime/*
+// @require https://jav-user.github.io/acescript/common/nes-1.0.2.js
+// @grant none
+// ==/UserScript==
+
+$(".custom-links").remove();
+async function getLinks() {
+	var $episodes = Array.from($("#episodeList li"));
+	for (var ep of $episodes) {
+		var link = $(ep).find("a").prop("href");
+
+		await $.get(link).then((html) => {
+			var $links = Array.from($(html).find("a.Button.Sm.fa-download"));
+			//console.log($links)
+			var links = $links
+				.map((a) => a.href)
+				.sort()
+				.map((link) => {
+					var host = new URL(link).hostname;
+					var arr = host.split(".");
+					var host = arr.slice(arr.length - 2).join(".");
+
+					var $link = $(
+						`
+						<div> &nbsp;   
+							<a class="custom-link"
+								target="_blank"
+								data-host="${host}" 
+								href="${link}">${host}
+							</a> |
+							<a 
+								class="copy-all"
+								href="javascript:;"
+								style="color:purple"
+								title="copy all ${host}"
+								>
+								copy all
+							</a> |
+							<a 
+								class="copy" 
+								href="javascript:;"
+								title="copy ${link}"
+								style="color:blue">copy
+							</a>
+						</div>
+						<br/>`.trim()
+					);
+
+					$link.find(".copy").on("click", function () {
+						new nstring(link).copy();
+						alert(`${host} copied!`);
+					});
+
+					$link.find(".copy-all").on("click", function () {
+						// var links = Array.from($(".custom-link")).map(a=>a.href).reverse().filter()
+						var links = Array.from($(`*[data-host="${host}"]`))
+							.reverse()
+							.map((a) => a.href);
+
+						console.log(links);
+						new nstring(links.join("\n")).copy();
+						alert(`${links.length} ${host} links copied!`);
+					});
+
+					return $link;
+				});
+			var $div = $(`<div class='custom-links'></div>`);
+			links.forEach((link) => $div.append(link));
+
+			$(ep).after($div.hide().fadeIn(2000));
+		});
+	}
+}
+
+$(document).ready(function () {
+	setTimeout(() => {
+		getLinks();
+		console.log("ready...");
+	}, 1 * 1000);
+});
--- subdivx/subtitles.js
+++ subdivx/subtitles.js
@@ -2,10 +2,18 @@
 // @name Subdivx
 // @namespace Subdivx Scripts
 // @match https://www.subdivx.com/index.php
+// @require https://jav-user.github.io/acescript/common/nes-1.0.2.js
 // @grant none
 // ==/UserScript==
 
 //clear()
+//
+//
+
+function timeout(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 var $links = Array.from($("a.titulo_menu_izq"));
 
 var $foots = $("div#buscador_detalle_sub_datos");
@@ -18,6 +26,7 @@ async function getLinks() {
 			var $html = $(res.trim());
 			var $link = $html.find("a.link1");
 			var href = $link.prop("href");
+			var host = new URL(href).hostname;
 			console.log(href);
 			// var data = $html.find("div#detalle_datos_derecha");
 			var $desc = $html.find("#detalle_datos font");
@@ -30,18 +39,54 @@ async function getLinks() {
 					`| 
 				<b>
 					<a
+                        class="custom_download"
 						title="${desc}" 
 						target="_blank"
 						href="${href}">
-							descargar
+							descargar (${host})
 					</a>
 				</b>`
 				)
 				.children(":last")
 				.hide()
-				.fadeIn(2000);
+				.fadeIn(2000)
+				.find("a")
+				.css("color", host == "www.subdivx.com" ? "green" : "red");
 		});
+	}
+
+	///  var $btn = $("<button>Download All</button>")
+
+	var $head = $(".result_busc3");
+
+	$head
+		.append(`<button>Download All</button>`)
+		.children(":last")
+		.hide()
+		.fadeIn(2000)
+		.on("click", downloadAll);
+	$head
+		.append(`<button>Copy All</button>`)
+		.children(":last")
+		.hide()
+		.fadeIn(2000)
+		.on("click", copyAll);
+}
+
+async function downloadAll() {
+	var $links = Array.from($("a.custom_download"));
+
+	for (var link of $links) {
+		await timeout(1000);
+		link.click();
 	}
 }
 
+function copyAll() {
+	var links = Array.from($("a.custom_download"))
+		.map((link) => link.href)
+		.join("\n");
+	new nstring(links).copy();
+	console.log("links", links);
+}
 getLinks();
