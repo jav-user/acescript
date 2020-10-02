8ad66120925c3bbc31d0ffef066a4148ec134c80
javuser <javuser@gmail.com>
2020-10-02 13:25:46 -0500

    animeflv.net episodes dev =

---
+++ animeflv/episodes.prod.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name AnimeFlvEps
+// @namespace AnimeFlvEps Scripts
+// @match https://www3.animeflv.net/anime/*
+// @require https://jav-user.github.io/acescript/common/nes-1.0.2.js
+// @require https://jav-user.github.io/acescript/animeflv/episodes.dev.js
+// @grant none
+// ==/UserScript==
