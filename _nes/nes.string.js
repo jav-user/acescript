e40cf9c3a51dc6464ffda90ea773e0448f6cb6c0
javuser <javuser@gmail.com>
2020-12-03 00:07:42 -0500

    vkvids

--- _nes/nes.string.js
+++ _nes/nes.string.js
@@ -1,109 +1,106 @@
 class nstring {
-    constructor(str) {
-        this.val = str;
-    }
-    capitalize() {
-        this.val = this.val
-            .split(" ")
-            .map((word) => word.charAt(0).toUpperCase() + word.substr(1))
-            .join(" ");
+	constructor(str) {
+		this.val = str;
+	}
+	capitalize() {
+		this.val = this.val
+			.split(" ")
+			.map((word) => word.charAt(0).toUpperCase() + word.substr(1))
+			.join(" ");
 
-        return this;
-    }
+		return this;
+	}
 
-    copy() {
-        var tempInput = document.createElement("textarea");
-        tempInput.value = this.val;
-        document.body.appendChild(tempInput);
-        tempInput.select();
-        document.execCommand("copy");
-        document.body.removeChild(tempInput);
-        return true;
-    }
+	copy() {
+		var tempInput = document.createElement("textarea");
+		tempInput.value = this.val;
+		document.body.appendChild(tempInput);
+		tempInput.select();
+		document.execCommand("copy");
+		document.body.removeChild(tempInput);
+		return true;
+	}
 
-    fileSize(_type) {
-        var fileSizeRx =
-            "[0-9]{1,}[.]{0,1}[0-9]{0,}[ ]{0,}[ ]{0,}[kmg]{0,1}[i]{0,1}[b]";
-        var str = this.val.trim().toLowerCase();
-        str = str.replace("bytes", "b");
+	fileSize(_type) {
+		var fileSizeRx =
+			"[0-9]{1,}[.]{0,1}[0-9]{0,}[ ]{0,}[ ]{0,}[kmg]{0,1}[i]{0,1}[b]";
+		var str = this.val.trim().toLowerCase();
+		str = str.replace("bytes", "b");
 
-        var match = "^" + fileSizeRx + "$";
-        if (!str.match(match)) return false;
+		var match = "^" + fileSizeRx + "$";
+		if (!str.match(match)) return false;
 
-        var num = str.match(/^[0-9]{1,}[.]{0,1}[0-9]{0,}/)[0];
-        num = Number.parseFloat(num);
+		var num = str.match(/^[0-9]{1,}[.]{0,1}[0-9]{0,}/)[0];
+		num = Number.parseFloat(num);
 
-        var type = str.match(/[kmg]{0,1}[i]{0,1}[b]$/)[0].replace("i", "");
+		var type = str.match(/[kmg]{0,1}[i]{0,1}[b]$/)[0].replace("i", "");
 
-        var types = {
-            b: 1,
-            kb: Math.pow(10, 3),
-            mb: Math.pow(10, 6),
-            gb: Math.pow(10, 9),
-        };
+		var types = {
+			b: 1,
+			kb: Math.pow(10, 3),
+			mb: Math.pow(10, 6),
+			gb: Math.pow(10, 9),
+		};
 
-        if (!_type) return num * types[type];
+		if (!_type) return num * types[type];
 
-        return (num * types[type]) / types[_type.toLowerCase()];
-    }
+		return (num * types[type]) / types[_type.toLowerCase()];
+	}
 
-    lower() {
-        this.val = this.val.toLowerCase();
-        return this;
-    }
+	lower() {
+		this.val = this.val.toLowerCase();
+		return this;
+	}
 
-    upper() {
-        this.val = this.val.toUpperCase();
-        return this;
-    }
+	upper() {
+		this.val = this.val.toUpperCase();
+		return this;
+	}
 
-    upperCount() {
-        var i = 0;
-        this.val.forEach((ch) => {
-            if (ch.toUpperCase() == ch) i++;
-        });
-        return i;
-    }
-    splitAll() {
-        this.val = this.val.split(/[-., ]/);
-        return this.val;
-    }
-    splitUpper() {
-        this.val = this.val.split(/(?=[A-Z])/);
-        return this.val;
-    }
+	upperCount() {
+		var i = 0;
+		this.val.forEach((ch) => {
+			if (ch.toUpperCase() == ch) i++;
+		});
+		return i;
+	}
+	splitAll() {
+		this.val = this.val.split(/[-., ]/);
+		return this.val;
+	}
+	splitUpper() {
+		this.val = this.val.split(/(?=[A-Z])/);
+		return this.val;
+	}
 
-    validFN() {
-        this.val = this.val
-            .replace(/[?]/g, "_")
-            .replace(/[:]/g, "-")
-            .replace(/[;]/g, ",")
-            .replace(/[*]/g, "")
-            .replace(/[/]/g, "-")
-            .replace(/[\\]/g, "-")
-            .replace(/[{]/g, "[")
-            .replace(/[}]/g, "]")
-            .replace(/["]/g, "'")
-            .replace(/[>]/g, "]")
-            .replace(/[<]/g, "[")
-            .replace(/[|]/g, "-");
-        return this;
-    }
+	validFN() {
+		this.val = this.val
+			.replace(/[?]/g, "_")
+			.replace(/[:]/g, "-")
+			.replace(/[;]/g, ",")
+			.replace(/[*]/g, "")
+			.replace(/[/]/g, "-")
+			.replace(/[\\]/g, "-")
+			.replace(/[{]/g, "[")
+			.replace(/[}]/g, "]")
+			.replace(/["]/g, "'")
+			.replace(/[>]/g, "]")
+			.replace(/[<]/g, "[")
+			.replace(/[|]/g, "-");
+		return this;
+	}
 
-    includesEvery(arr) {
+	includesEvery(arr) {
+		let includes = (el) => this.val.includes(el + "");
+		return arr.every(includes);
+	}
 
-        let includes = (el) => this.val.includes((el + ""));
-        return arr.every(includes);
-    }
+	includesSome(arr) {
+		let includes = (el) => this.val.includes(el + "");
+		return arr.some(includes);
+	}
 
-    includesSome(arr) {
-
-        let includes = (el) => this.val.includes((el + ""));
-        return arr.some(includes);
-    }
-
-
-    exec() {
-        return this.val;
-    }
+	exec() {
+		return this.val;
+	}
 }
--- vk/vk.videos.js
+++ vk/vk.videos.js
@@ -2,92 +2,168 @@
 // @name VKVids
 // @namespace VKVids Script
 // @match https://vk.com/search
+// @match https://vk.com/videos-*
 // @require https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
-// @require https://jav-user.github.io/acescript/common/nes-1.0.2.js
+// @require https://jav-user.github.io/acescript/common/nes-1.0.2.js?a=1
 // @grant none
 // ==/UserScript==
 
-function createGUIHeader() {
+var $ = jQuery.noConflict();
+var nid = 0;
+var sort_order = false;
 
-    $("#search_header").before(`<div>
+function copySelected() {
+  var selections = [];
+  $(".nselectv:checked").each((i, selection) => {
+    selections.push($(selection).val());
+  });
+  new nstring(selections.join("\n")).copy();
+  alert(selections.length + " links copied!");
+}
+
+function createGUIHeader() {
+  const header = `<div>
       <input id="nselectv" 
       type="checkbox"/> Select All
+      <button type="button" id="sort-videos">sort</button>
       <button type="button" id="copy-selected">copy selected</button>
-    </div>`)
+    
+    </div>`;
 
-    $("#nselectv").on("change", function() {
-        var checked = this.checked
-        console.log("checked", checked)
-        $(".nselectv").prop("checked", checked).trigger("change");
-    })
+  if ($("#search_header").length) {
+    $("#search_header").before(header);
+  } else if ($(".ui_search_new").length) {
+    $(".ui_search_new").before(header);
+  }
 
-    $("#copy-selected").on("click", function() {
-        var selections = []
-        $(".nselectv:checked").each((i, selection) => {
-            selections.push($(selection).val())
-        })
-        new nstring(selections.join("\n")).copy()
-        alert(selections.length +" links copied!")
+  $("#nselectv").on("change", function () {
+    var checked = this.checked;
+    console.log("checked", checked);
+    $(".nselectv:not(.nblacklisted)")
+      .prop("checked", checked)
+      .trigger("change");
+  });
+
+  $("#copy-selected").on("click", copySelected);
+  $("#sort-videos").on("click", sortVideos);
+}
+
+function sortVideos() {
+  $(".video_item")
+    .sort(function (a, b) {
+      var A = $(a).data("seconds");
+      var B = $(b).data("seconds");
+
+      return parseInt(A) < parseInt(B);
     })
+    .appendTo($(".video_item").eq(0).parent());
+
+  console.log($(".video_item.ndev").eq(0).parent());
 }
 
 function createGUIItems() {
-    if (!$(".video_item:not(.ndev)")) return false;
-    $(".video_item:not(.ndev)").each((i, item) => {
-        var title = $(item).find(".video_item_title").text().trim()
-        var url = $(item).find(".video_item_title").prop("href").trim()
+  var $items = $(".video_item:not(.ndev)");
+  if (!$items) return false;
+  if (!$items.length) return false;
 
-        $(item).append(`
+  $items.each((i, item) => {
+    var time = $(item).find(".video_thumb_label_item").text().trim().split(":");
+    var seconds = 0;
+    time.reverse().forEach((num, i) => {
+      seconds += num * Math.pow(60, i);
+    });
+
+    $(item).attr("data-seconds", seconds);
+    $(item).attr("data-nid", nid);
+    nid++;
+    var title = $(item).find(".video_item_title").text().trim();
+    var url = $(item).find(".video_item_title").prop("href").trim();
+    var _title = $(item).find("a").text().toLowerCase();
+
+    $(item)
+      .append(
+        `
   <div 
     title="Select ${title}"
     class="ndev"
     >
     <input
-      data-item="${$(item).attr('id')}"
+      data-item="${$(item).attr("id")}"
       value="${url}"
       class="nselectv"
       type="checkbox"/>Seleccionar
+  <button class="copy-selected">copy selected</button>
   </div>
-`).addClass("ndev")
+`
+      )
+      .addClass("ndev");
 
-    })
+    if (
+      new nstring(_title).includesSome([
+        "gay",
+        "transs",
+        "boyfun",
+        "men.com",
+        "nakedsword",
+        "tranny",
+        "shemale",
+        "[men]"
+      ])
+    ) {
+      /*  if(_title.includes("gay") 
+          || _title.includes("transs")
+          || _title.includes("boyfun")
+         || _title.includes("boy fun")
+         || _title.includes("men.com")
+         || _title.includes("nakedsword")
+         || _title.includes("naked sword")
+           || _title.includes("tranny")
+         || 
+        )
+        */
+      $(item).find(".video_item_thumb").css("opacity", 0.1);
+      $(item).find("a").css("color", "gray");
+      $(item).find(".nselectv").addClass("nblacklisted");
+    }
+  });
+  if (!$(".nselectv:not(.nfunction)") || !$(".nselectv:not(.nfunction)").length)
+    return false;
+  $(".nselectv:not(.nfunction)").each((i, select) => {
+    var $item = $("#" + $(select).attr("data-item"));
+    //console.log($item)
 
-    $(".nselectv:not(.nfunction)").each((i, select) => {
-        var $item = $("#" + $(select).attr("data-item"))
-        //console.log($item)
-        
-        $(select).on("change", function() {
-          var checked = this.checked
-            $item.toggleClass("nchecked", checked)
-            $(select).parent().toggleClass('nchecked', checked);
-            var selected=$(".nselectv:checked").length;
-            $("#copy-selected").text(`copy selected (${selected})`)
-        })
-        $(select).addClass("nfunction")
-    })
+    $(select).on("change", function () {
+      var checked = this.checked;
+      $item.toggleClass("nchecked", checked);
+      $(select).parent().toggleClass("nchecked", checked);
+      var selected = $(".nselectv:checked").length;
+      $("#copy-selected").text(`copy selected (${selected})`);
+    });
+    $(select).addClass("nfunction");
+  });
 
+  $(".copy-selected:not(.nfunction)")
+    .on("click", copySelected)
+    .addClass("nfunction");
 }
 
-
 function createStyles() {
-
-    $("head").append(`<style>
+  $("head").append(`<style>
 .nchecked{
   background-color: #cce9ff !important;
 }
-</style>`)
-
+</style>`);
 }
 
 $(document).ready(() => {
+  //$(".video_item.video_can_add").fadeOut(2000)
+  createGUIHeader();
 
-    createGUIHeader();
+  setInterval(() => {
+    createGUIItems();
 
-    setInterval(() => {
+    //        $(".video_item.video_can_add").remove()
+  }, 5 * 1000);
 
-        createGUIItems()
-
-    }, 5 * 1000)
-
-    createStyles();
-})
+  createStyles();
+});
